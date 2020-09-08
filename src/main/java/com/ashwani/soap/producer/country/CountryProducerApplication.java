aec0b20b16bbba2208ac345e273ed70e443e067f
Ashwani Solanki <solanki.ashwani@yahoo.com>
2020-09-08 11:32:12 +0530

    Initial commit

---
+++ src/main/java/com/ashwani/soap/producer/country/CountryProducerApplication.java
@@ -0,0 +1,13 @@
+package com.ashwani.soap.producer.country;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountryProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountryProducerApplication.class, args);
+	}
+
+}
